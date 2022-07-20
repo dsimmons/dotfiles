072b9ea484a5b6b4a83148efb0b2ff0f9fc5ee71
Dan Simmons <dan@simmons.io>
2022-07-19 19:08:55 -0600

    Replaced vim gruvbox theme with modern Lua port.
    Integrates with Neovim/Lua better, supports Treesitter, etc...

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,8 +1,12 @@
 -- Bootstrap Packer if not present.
+-- https://github.com/wbthomason/packer.nvim#bootstrapping
 local fn = vim.fn
 local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+  packer_bootstrap = fn.system({
+    "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim",
+    install_path
+  })
 end
 
 -- Ensure `packer` can be required when first bootstrapping.
@@ -71,7 +75,7 @@ return require("packer").startup(function(use)
   use "tpope/vim-surround"
 
   -- Use a better colorscheme.
-  use "morhetz/gruvbox"
+  use { "ellisonleao/gruvbox.nvim" }
 
   -- The plugin that I use for my `todo.txt` file for now.
   -- See: http://todotxt.org/
