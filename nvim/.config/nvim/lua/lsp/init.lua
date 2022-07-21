d3331c6d195d8a683c78b85da09bd17f555dfdfd
Dan Simmons <dan@simmons.io>
2022-07-21 15:35:37 -0600

    Changed LSP keymap config grouping, more coherent.
    What was there prior to this commit was largely copy/pasted from
    nvim-lspconfig's recommended mappings. I rearranged the lines to be more
    logically grouped by key chord (versus however it was organized before).

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -6,34 +6,34 @@ local bind = vim.keymap.set
 
 local diagnostic = vim.diagnostic
 local opts = { silent=true }
-bind('n', '<space>e', diagnostic.open_float, opts)
 bind('n', '[d', diagnostic.goto_prev, opts)
 bind('n', ']d', diagnostic.goto_next, opts)
+bind('n', '<space>e', diagnostic.open_float, opts)
 bind('n', '<space>q', diagnostic.setloclist, opts)
 
 -- Set up mappings using a callback when an LSP server attaches to the current
 -- buffer.
 local buf = vim.lsp.buf
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   local bufopts = { silent=true, buffer=bufnr }
-  bind('n', 'gD', buf.declaration, bufopts)
   bind('n', 'gd', buf.definition, bufopts)
-  bind('n', 'K', buf.hover, bufopts)
+  bind('n', 'gD', buf.declaration, bufopts)
   bind('n', 'gi', buf.implementation, bufopts)
+  bind('n', 'gr', buf.references, bufopts)
+  bind('n', 'K', buf.hover, bufopts)
   bind('n', '<C-k>', buf.signature_help, bufopts)
+  bind('n', '<space>ca', buf.code_action, bufopts)
+  bind('n', '<space>D', buf.type_definition, bufopts)
+  bind('n', '<space>f', buf.formatting, bufopts)
+  bind('n', '<space>rn', buf.rename, bufopts)
   bind('n', '<space>wa', buf.add_workspace_folder, bufopts)
-  bind('n', '<space>wr', buf.remove_workspace_folder, bufopts)
   bind('n', '<space>wl', function()
     print(vim.inspect(buf.list_workspace_folders()))
   end, bufopts)
-  bind('n', '<space>D', buf.type_definition, bufopts)
-  bind('n', '<space>rn', buf.rename, bufopts)
-  bind('n', '<space>ca', buf.code_action, bufopts)
-  bind('n', 'gr', buf.references, bufopts)
-  bind('n', '<space>f', buf.formatting, bufopts)
+  bind('n', '<space>wr', buf.remove_workspace_folder, bufopts)
 end
 
 local enabled_lsps = {
