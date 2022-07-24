9c489641f55ce6fc2b66faba5bd012f8fd1d40b0
Dan Simmons <dan@simmons.io>
2022-07-23 19:25:53 -0600

    Added convenient nvim mapping for setting `pwd`.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,11 +6,11 @@ local silent = { silent = true }
 -- Modified from: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#falling-back-to-find_files-if-git_files-cant-find-a-git-directory
 --
 -- If the buffer being editited is part of a git repository, use that as the
--- project root (versus relative to the buffer or cwd). Otherwise, use the
--- default relative CWD behavior.
+-- project root (versus relative to the pwd). Otherwise, fall back to the
+-- default `:pwd` behavior.
 --
--- Allows for opening a specific file (e.g. `nvim src/index.ts`) and still being
--- able to search over files in the project's root (e.g. `package.json`).
+-- Where applicable (Git repo), it makes finding files independent of the
+-- spawning terminal's `pwd` and/or allows for using `set autochdir`.
 local function project_files()
   local ok = pcall(require('telescope.builtin').git_files)
   if not ok then require('telescope.builtin').find_files() end
--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -8,6 +8,12 @@ bind('n', '<C-j>', '<C-w>j', opts_silent)
 bind('n', '<C-k>', '<C-w>k', opts_silent)
 bind('n', '<C-l>', '<C-w>l', opts_silent)
 
+-- Change `:pwd` to be the dir of the file being edited, in cases where
+-- `set autochdir` isn't feasible or appropriate.
+--
+-- `:lcd` (vs `:cd`) only affects the current buffer.
+bind('n', '<Leader>cd', ':lcd %:p:h<CR>')
+
 -- Convenient chords for editing and reloading the nvim config file.
 bind('n', '<Leader>ve', ':e $MYVIMRC<CR>')
 bind('n', '<Leader>vr', ':source $MYVIMRC<CR>')
