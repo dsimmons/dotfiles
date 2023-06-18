d51b5cf755910243dcce4d406dbc4a782abe0976
Dan Simmons <dan@simmons.io>
2023-06-17 21:18:32 -0600

    Changed nvim formatters installed by default.

--- nvim/.config/astronvim/lua/user/plugins/mason.lua
+++ nvim/.config/astronvim/lua/user/plugins/mason.lua
@@ -26,8 +26,11 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "eslint_d",
+        "prettierd",
+        "shellcheck",
+        "shfmt",
+        "stylua",
       })
     end,
   },
