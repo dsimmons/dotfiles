c6013dcbfe51e904e8cff5bacc304cc30826ed5e
Dan Simmons <dan@simmons.io>
2023-06-17 21:23:30 -0600

    Enabled AstroNvim community pack, Typescript.

--- nvim/.config/astronvim/lua/user/plugins/community.lua
+++ nvim/.config/astronvim/lua/user/plugins/community.lua
@@ -4,6 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.typescript" },
+
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
