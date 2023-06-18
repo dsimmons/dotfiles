28d8e27b3b90bf050192aaca6f3815db5f87615c
Dan Simmons <dan@simmons.io>
2023-06-17 21:15:54 -0600

    Added nvim language servers installed by default.

--- nvim/.config/astronvim/lua/user/plugins/mason.lua
+++ nvim/.config/astronvim/lua/user/plugins/mason.lua
@@ -7,7 +7,15 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "bashls",
+        "cssls",
+        "dockerls",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "solidity_ls_nomicfoundation",
+        "tailwindcss",
+        "yamlls"
       })
     end,
   },
