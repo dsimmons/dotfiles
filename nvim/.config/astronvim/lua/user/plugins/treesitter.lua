3d0a4594aaa7a52c15b0159ffe8209df0f48b051
Dan Simmons <dan@simmons.io>
2023-06-17 20:53:15 -0600

    Added a host of Treesitter grammars by default.
    It may make more sense to enable `auto_install` at this point, but I
    figured having this list of grammars that I want always enabled (at
    minimum) would come in handy!

--- nvim/.config/astronvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/astronvim/lua/user/plugins/treesitter.lua
@@ -3,7 +3,30 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "bash",
+      "clojure",
+      "css",
+      "dockerfile",
+      "git_config",
+      "git_rebase",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "graphql",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "solidity",
+      "todotxt",
+      "toml",
+      "tsx",
+      "typescript",
+      "yaml"
     })
   end,
 }
