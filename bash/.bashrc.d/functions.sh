d2dd880f6403e7a0f0f458754b4c406ae22daaae
Dan Simmons <dan@simmons.io>
2024-05-12 12:22:51 -0600

    Added sh function for one-off encrypted backups.
    See comment, an additional backup mechanism in case my primary backups
    fail or become corrupted.

--- bash/.bashrc.d/functions.sh
+++ bash/.bashrc.d/functions.sh
@@ -26,3 +26,56 @@ gtd() {
 
   nvim "${PARAMS[@]}"
 }
+
+# A quick and dirty way to create an encrypted export of $HOME to store on
+# untrusted cloud storage.
+#
+# This is intended to be a secondary mechanism that acts as a conservative
+# sanity check of sorts, whereas we have better backup tools being run more
+# regularly (e.g. `kopia`).
+#
+# TL;DR: This is a backup backup, so that we don't have to place our trust in
+# any one tool (e.g. if Kopia repository gets corrupted).
+#
+# Currently assumes it's being run interactively, while the invoker is present
+# (symmetric encryption key).
+snapshot_homedir() {
+  BACKUP_DIR="/tmp/manual-snapshots"
+
+  BACKUP_FILE="$(date --rfc-3339=date)_${USER}@${HOSTNAME}.tar.zst"
+  ENCRYPTED_FILE="${BACKUP_FILE}.gpg"
+
+  BACKUP_PATH="${BACKUP_DIR}/${BACKUP_FILE}"
+  ENCRYPTED_PATH="${BACKUP_DIR}/${ENCRYPTED_FILE}"
+
+  # Ensure the necessary dependencies exist.
+  for cmd in tar zstd gpg; do
+    if ! command -v "$cmd" &>/dev/null; then
+      echo "$cmd could not be found. Please install it and try again."
+      return 1
+    fi
+  done
+
+  mkdir -p "${BACKUP_DIR}"
+
+  echo "Creating archive at ${BACKUP_PATH}..."
+  if ! tar --zstd -cpf "${BACKUP_PATH}" "${HOME}"; then
+    echo "Error: failed to create archive at ${BACKUP_PATH}."
+    return 1
+  fi
+
+  echo "Encrypting archive as ${ENCRYPTED_PATH}..."
+  if ! gpg --symmetric --cipher-algo AES256 "${BACKUP_PATH}"; then
+    echo "Error: failed to encrypt archive at ${BACKUP_PATH}."
+
+    # Clean up unencrypted file if encryption fails.
+    echo "Removing ${BACKUP_PATH}..."
+    rm "${BACKUP_PATH}"
+    return 1
+  fi
+
+  echo "Removing unencrypted archive ${BACKUP_PATH}..."
+  rm "${BACKUP_PATH}"
+
+  echo "Encrypted backup complete: ${ENCRYPTED_PATH}"
+}
